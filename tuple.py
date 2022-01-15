f458b580c102fb407267a349d7a6afd53f641687
DARSHMARVANIYA <88242199+DARSHMARVANIYA@users.noreply.github.com>
2022-01-15 22:54:56 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,106 @@
+#Tuple
+
+# a. Write a Python program to create a tuple with different data types.
+
+# tuple = ("Drash", 3.2, 1,False)
+# print(tuple)
+
+
+
+# b. Write a Python program to create a tuple 
+# with numbers and print one item.
+
+# colors = ("red", "pink", "blue", "green")
+# item = colors[2]
+# print(item)
+
+
+
+# c. Write a Python program to add an item in a tuple.
+
+# colors = ("red", "pink", "blue", "green")
+# print(colors)
+
+# A = list(colors)
+# A.append("white")
+# print(A)
+
+
+
+# d. Write a Python program to convert a tuple to a string.
+
+# tuple = ('d','a','r','s','h')
+# str = ''.join(tuple)
+# print(str)
+
+
+
+# e. Write a Python program to find the length of a tuple.
+
+# tuple = ('d','a','r','s','h')
+# print(len(tuple))
+
+# tup = tuple("Darsh Marvaniya")
+# print(tup)
+# print(len(tup))
+
+
+# Darsh Marvaniya
+# 20CS034
+# Github Link = #Tuple
+
+# a. Write a Python program to create a tuple with different data types.
+
+# tuple = ("Drash", 3.2, 1,False)
+# print(tuple)
+
+
+
+# b. Write a Python program to create a tuple 
+# with numbers and print one item.
+
+# colors = ("red", "pink", "blue", "green")
+# item = colors[2]
+# print(item)
+
+
+
+# c. Write a Python program to add an item in a tuple.
+
+# colors = ("red", "pink", "blue", "green")
+# print(colors)
+
+# A = list(colors)
+# A.append("white")
+# print(A)
+
+
+
+# d. Write a Python program to convert a tuple to a string.
+
+# tuple = ('d','a','r','s','h')
+# str = ''.join(tuple)
+# print(str)
+
+
+
+# e. Write a Python program to find the length of a tuple.
+
+# tuple = ('d','a','r','s','h')
+# print(len(tuple))
+
+# tup = tuple("Darsh Marvaniya")
+# print(tup)
+# print(len(tup))
+
+
+# Darsh Marvaniya
+# 20CS034
+# Github Link = https://github.com/DARSHMARVANIYA/Python-Practical.git
+
+
+
+
+
+
+
